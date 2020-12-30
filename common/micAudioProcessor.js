f0c42d796c72d54cb950e4bb591f3f260caffcc2
Cameron Martin <cameron.martin@forgerock.com>
2020-12-30 16:18:44 +0000

    Fix some weirdnesses

--- common/micAudioProcessor.js
+++ common/micAudioProcessor.js
@@ -52,13 +52,13 @@ export default class MicAudioProcessor {
         return before + (after - before) * atPoint;
       };
 
-      var newData = new Array();
-      var springFactor = new Number((data.length - 1) / (fitCount - 1));
+      var newData = [];
+      var springFactor = (data.length - 1) / (fitCount - 1);
       newData[0] = data[0]; // for new allocation
       for ( var i = 1; i < fitCount - 1; i++) {
         var tmp = i * springFactor;
-        var before = new Number(Math.floor(tmp)).toFixed();
-        var after = new Number(Math.ceil(tmp)).toFixed();
+        var before = Math.floor(tmp);
+        var after = Math.ceil(tmp);
         var atPoint = tmp - before;
         newData[i] = linearInterpolate(data[before], data[after], atPoint);
       }
